d95fa5885e925f755e37982b9f4bdf5df5dba246
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-11 21:43:09 +0000

    more pages added, not working

--- application/routes.py
+++ application/routes.py
@@ -8,3 +8,15 @@ from application.models import Posts
 def home():
     blogData = Posts.query.first()
     return render_template('home.html', title='Home Page', posts=blogData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Page')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login Page')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register Page')
