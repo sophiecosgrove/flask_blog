fbe7fcdf505197ca083f5f2b14c417d06e8ac316
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-14 11:31:32 +0000

    14/05/2020

--- application/models.py
+++ application/models.py
@@ -1,18 +1,19 @@
 from application import db
 from application import login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(30), nullable=False)
-    last_name = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(100), nullable=False, unique=True)
     content = db.Column(db.String(500), nullable=False, unique=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  
 
     def __repr__(self):
         return ''.join([
-            'User: ', self.first_name, ' ', self.last_name, '\r\n',
-            'Title: ', self.title, '\r\n', self.content
+             'User ID: ', self.user_id, '\r\n',
+             'Title: ', self.title, '\r\n', self.content
             ])
 
 @login_manager.user_loader
@@ -21,11 +22,17 @@ def load_user(id):
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(500), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
+    posts = db.relationship('Posts', backref='author', lazy=True)
 
     def __repr__(self):
-        return ''.join(['UserID: ', str(self.id), '\r\n',
-            'Email: ', self.email])
+        return ''.join([
+             'User ID: ', str(self.id), '\r\n',
+             'Email: ', self.email, '\r\n',
+             'Name: ', self.first_name, ' ', self.last_name
+        ])
 
 
